376bb590f06d1736d59f882a3fd026c519fab1e1
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-21 16:19:22 +0200

    ADD: correzione primo es.

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -7,7 +7,19 @@ console.log (names)
 
 // Dichiara la funzione qui.
 
-var newArray = names.map(name => name.charAt(0));
+//const newArray = names.map(name => name.charAt(0));
+
+function getFirstLetters(array) {
+    const letters = [];
+
+    for(let i = 0; i < array.length; i++) {
+        const name = array[i];
+        letters.push(name.charAt(0));
+    }
+
+    return letters;
+}
+console.log(getFirstLetters(names));
 
 // Invoca la funzione qui e stampa il risultato in console
 
