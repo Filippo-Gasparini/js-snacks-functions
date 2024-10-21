f4f503c2334d477f9731c8fd399e4823f440e1ab
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-21 18:02:41 +0200

    ADD: correzione

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,13 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
 const word = 'javascript';
-
+console.log(word)
 
 // Dichiara la funzione qui.
-
+const letters = word.charAt(1,3,7);
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(letters)
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
