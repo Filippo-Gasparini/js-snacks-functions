93bb049095f1318bd264070e03e2dbc588001ac6
Filippo Gasparini <filippogaspa02@gmail.com>
2024-10-21 14:57:14 +0200

    ADD: dichiaro la funzione

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,17 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array 
+con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
+console.log (names)
 
 
 // Dichiara la funzione qui.
 
+var newArray = names [''];
+console.log(newArray);
 
 // Invoca la funzione qui e stampa il risultato in console
-
+//console.log(newArray)
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
